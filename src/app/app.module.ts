b7adf613ffdc1d24029bd8e44a6ada54799e58b9
Darshan614 <68334937+Darshan614@users.noreply.github.com>
2023-05-23 12:34:08 +0530

    5 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,18 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainComponent } from './main/main.component';
 import { ContainerComponent } from './container/container.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ContainerComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+];
+
 @NgModule({
-  imports: [CommonModule, BrowserModule],
+  imports: [CommonModule, BrowserModule, RouterModule.forRoot(appRoutes)],
   declarations: [
     AppComponent,
     NavbarComponent,
