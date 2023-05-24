17c0e72f1ebe81c513827c528a4dc78cb33981ab
Darshan614 <68334937+Darshan614@users.noreply.github.com>
2023-05-24 12:11:03 +0530

    5 files modified

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+  tech: string = 'React';
+  techarr: string[] = [
+    'Angular',
+    'React',
+    'NodeJS',
+    'MongoDB',
+    'Express',
+    'AWS',
+  ];
+  id: any;
+  cindex: number = 0;
+  constructor() {}
 
   ngOnInit() {
+    this.id = setInterval(() => {
+      console.log('hi');
+      this.tech = this.techarr[this.cindex];
+      if (this.cindex >= 5) {
+        this.cindex = 0;
+      } else {
+        this.cindex++;
+      }
+    }, 2000);
   }
-
+  ngOnDestroy() {
+    clearInterval(this.id);
+  }
 }
